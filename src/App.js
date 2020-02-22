558f5df06ce833e07f02478b4630a4a640f0d32f
wili1001 <wilirahmatm@gmail.com>
2020-02-22 11:09:58 +0800

    coba update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Coba React 2020</p>
+        <p>Coba Update Web</p>
         <a
           className="App-link"
           href="https://reactjs.org"
