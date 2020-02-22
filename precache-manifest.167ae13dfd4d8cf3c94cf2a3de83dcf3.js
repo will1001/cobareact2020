bed604d158aa575865d7a56f63ad9f4a798ac8ea
wili1001 <wilirahmatm@gmail.com>
2020-02-22 11:06:53 +0800

    Updates

---
+++ precache-manifest.167ae13dfd4d8cf3c94cf2a3de83dcf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683392dafc184973437b641591427354",
+    "url": "/cobareact2020/index.html"
+  },
+  {
+    "revision": "6709628a304237c8c520",
+    "url": "/cobareact2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/cobareact2020/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cobareact2020/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6709628a304237c8c520",
+    "url": "/cobareact2020/static/js/main.014776d7.chunk.js"
+  },
+  {
+    "revision": "b12a93773ddbd652f8a0",
+    "url": "/cobareact2020/static/js/runtime-main.deb46c2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cobareact2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
